08a6df4a2493ea8c6cabf5c7e8e97f87a8802f9a
banagaurav <gauravbana840@gmail.com>
2025-10-10 13:25:36 +0545

    added skills in constants

--- api/src/constants.ts
+++ api/src/constants.ts
@@ -2,3 +2,6 @@ export const SEQUELIZE = 'SEQUELIZE';
 export const DEVELOPMENT = 'development';
 export const TEST = 'test';
 export const PRODUCTION = 'production';
+
+export const SKILLS_REPOSITORY = 'SKILLS_REPOSITORY';
+export const SKILLS_CATEGORIES_REPOSITORY = 'SKILLS_CATEGORIES_REPOSITORY';
