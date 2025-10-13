5d318a3f38440fb7f84bb53d4fdab62d0d04ca7f
dipojha <dipojha03@gmail.com>
2025-10-13 22:46:32 +0545

    header done

--- front/src/components/layout/Header.jsx
+++ front/src/components/layout/Header.jsx
@@ -1,14 +1,19 @@
+import headerIcon from "@/assets/icons/header-icon.svg";
+import Image from "next/image";
 export default function Header() {
    return (
       <header className="w-full  text-white py-4 ">
-         <div className="flex justify-center items-center space-x-10 text-sm font-light">
+         <div className="flex justify-center items-center space-x-15 text-lg">
             <span className="text-gray-300">Tools</span>
             <span className="text-gray-300">Frontend</span>
 
             <div className="text-center leading-tight">
-               <span className="text-yellow-400 text-lg font-serif block -mb-1">Gaurav</span>
-               <span className="text-white text-lg font-serif">Raj </span>
-               <span className="text-yellow-400 text-lg font-serif">Bana</span>
+               <Image
+                  src={headerIcon}
+                  alt="Laptop Icon"
+                  width={200}
+                  height={200}                
+               />
             </div>
 
             <span className="text-gray-300">Backend</span>
