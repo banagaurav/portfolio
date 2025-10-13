e6a4f86527aac4a51f2349fd42b846339136ba49
banagaurav <gauravbana840@gmail.com>
2025-10-13 21:57:28 +0545

    added header logo

--- front/src/components/layout/Footer.jsx
+++ front/src/components/layout/Footer.jsx
@@ -1,7 +1,10 @@
 import { memo } from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  /*************  ✨ Windsurf Command 🌟  *************/
+  return <div className="bg-red">Footer</div>;
+  return <div style={{ backgroundColor: "red" }}>Footer</div>;
+  /*******  1cefbf8a-91ab-43fe-b37b-1023fac6b9a6  *******/
 };
 
 export default memo(Footer);
