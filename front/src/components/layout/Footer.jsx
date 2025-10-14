cc49bb9a8dcc999c64993f2db0a72680790819e9
banagaurav <gauravbana840@gmail.com>
2025-10-14 09:25:50 +0545

    refactor: update Footer component styling for improved visibility

--- front/src/components/layout/Footer.jsx
+++ front/src/components/layout/Footer.jsx
@@ -1,10 +1,7 @@
 import { memo } from "react";
 
 const Footer = () => {
-  /*************  ✨ Windsurf Command 🌟  *************/
-  return <div className="bg-red">Footer</div>;
-  return <div style={{ backgroundColor: "red" }}>Footer</div>;
-  /*******  1cefbf8a-91ab-43fe-b37b-1023fac6b9a6  *******/
+  return <div className="bg-red-500 text-white">Footer</div>;
 };
 
 export default memo(Footer);
