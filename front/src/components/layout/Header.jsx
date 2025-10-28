c7ebc451906ad381abd8518ace2babcd7fd790b4
dipojha <dipojha03@gmail.com>
2025-10-28 23:22:41 +0545

    header  fixed

--- front/src/components/layout/Header.jsx
+++ front/src/components/layout/Header.jsx
@@ -1,23 +1,19 @@
 import headerIcon from "@/assets/icons/header-icon.svg";
 import Image from "next/image";
+
 export default function Header() {
    return (
-      <header className="w-full  text-white py-4 ">
-         <div className="flex justify-center items-center space-x-15 text-lg">
-            <span className="text-gray-300">Tools</span>
-            <span className="text-gray-300">Frontend</span>
+      <header className="sticky top-0 w-full bg-black text-white py-4 z-50">
+         <div className="flex justify-center items-center space-x-10 text-lg">
+            <span className="text-gray-300 cursor-pointer hover:text-white transition">Tools</span>
+            <span className="text-gray-300 cursor-pointer hover:text-white transition">Frontend</span>
 
             <div className="text-center leading-tight">
-               <Image
-                  src={headerIcon}
-                  alt="Laptop Icon"
-                  width={200}
-                  height={200}                
-               />
+               <Image src={headerIcon} alt="Laptop Icon" width={200} height={200} />
             </div>
 
-            <span className="text-gray-300">Backend</span>
-            <span className="text-gray-300">Database</span>
+            <span className="text-gray-300 cursor-pointer hover:text-white transition">Backend</span>
+            <span className="text-gray-300 cursor-pointer hover:text-white transition">Database</span>
          </div>
       </header>
    );
