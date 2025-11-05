dcadf62d4010b59d135c9bcc643669328b0e7fe7
banagaurav <gauravbana840@gmail.com>
2025-11-05 22:58:15 +0545

    changed

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -3,6 +3,7 @@ import Footer from "@/components/layout/Footer/Footer";
 import Header from "@/components/layout/Header";
 
 export default function Home() {
+   // adsf
   return (
    <>
    <Header/>
